0ff1920e910fdd34d18ecf5eb2d1d13f6a31ff91
Cameron Ayer <camayer92@gmail.com>
2018-03-27 10:23:51 +0900

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -9,18 +9,6 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// app.post('/todos', (req, res) => {
-//   var todo = new Todo({
-//     text: req.body.text
-//   });
-//
-//   todo.save().then((doc) => {
-//     res.send(doc);
-//   }, (e) => {
-//     res.status(400).send(e);
-//   });
-// });
-
 app.post('/todos', (req, res) => {
   var myTodo = new Todo({
     text: req.body.text
@@ -38,3 +26,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 })
+
+module.exports = {app};
